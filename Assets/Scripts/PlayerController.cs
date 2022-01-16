33e9f68f47cc607ed0f65c80d6cf66293a147c61
arunprksm <97307361+arunprksm@users.noreply.github.com>
2022-01-16 16:18:59 +0530

    script updated with Player Flip function with run animation...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
+    float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerFlip();
+        //PlayerJump();
+    }
+
+    void PlayerFlip()
+    {
+        speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        Vector3 playerFlip = transform.localScale;
+        if (speed < 0) playerFlip.x = -1f * Mathf.Abs(playerFlip.x);
+        else if (speed > 0) playerFlip.x = Mathf.Abs(playerFlip.x);
+        transform.localScale = playerFlip;
     }
 }
