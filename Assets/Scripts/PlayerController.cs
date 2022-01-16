7a9b6f01dced35f83889fe684228adb7435cbb15
arunprksm <97307361+arunprksm@users.noreply.github.com>
2022-01-17 02:13:37 +0530

    Binded horizontal input to walk the player and move it forward...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    float speed;
+
+    float playerHorizontal;
+    public float playerSpeed;
+    bool crouch, jump;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerFlip();
+        playerHorizontal = Input.GetAxisRaw("Horizontal");
+
+        PlayerMovement(playerHorizontal);
+        PlayerFlip(playerHorizontal);
         //PlayerJump();
+        PlayerCrouch();
+    }
+    void PlayerMovement(float playerHorizontal)
+    {
+        Vector3 playerMovement = transform.position;
+        playerMovement.x += playerHorizontal * playerSpeed * Time.deltaTime;
+        transform.position = playerMovement;
+    }
+    void PlayerFlip(float playerHorizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(playerHorizontal));
+        Vector3 playerFlip = transform.localScale;
+        if (playerHorizontal < 0) playerFlip.x = -1f * Mathf.Abs(playerFlip.x);
+        else if (playerHorizontal > 0) playerFlip.x = Mathf.Abs(playerFlip.x);
+        transform.localScale = playerFlip;
     }
 
-    void PlayerFlip()
+    //void PlayerJump()
+    //{
+
+    //}
+
+    void PlayerCrouch()
     {
-        speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-        Vector3 playerFlip = transform.localScale;
-        if (speed < 0) playerFlip.x = -1f * Mathf.Abs(playerFlip.x);
-        else if (speed > 0) playerFlip.x = Mathf.Abs(playerFlip.x);
-        transform.localScale = playerFlip;
+        crouch = Input.GetKeyDown(KeyCode.C);
     }
 }
